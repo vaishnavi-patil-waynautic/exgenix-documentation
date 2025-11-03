01f5e18235095f30466b567de1f23cb4cc7118d0
Vaishnavi Patil <vaishnavipatil978@gmail.com>
2025-11-03 22:07:48 +0530

    Deployment Files

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -183,18 +183,14 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://exgenix.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+   url: 'https://vaishnavi-patil-waynautic.github.io',
+  baseUrl: '/exgenix-documentation/',
+  organizationName: 'vaishnavi-patil-waynautic',
+  projectName: 'exgenix-documentation',
+  deploymentBranch: 'gh-pages',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'exgenix', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
